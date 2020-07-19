405d7fbdd830dab080fffd8d8136ebf4eb31f96c
Sanskriti-Amidesh <67536985+Sanskriti-Amidesh@users.noreply.github.com>
2020-07-19 10:17:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+function copy() {
+    alert('LOL!!! Click, OK to continue!!!');
+}
+
+function summer() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/dkd5jyhic/image/upload/v1594968327/Summer_tf9yzu.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("It is SCORCHING HOT OUTSIDE.");
+}
+
+function winter() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("It is SO COLD, I CAN BARELY BREATHE.");
+}
+
+function spring() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("It is SPRING, FLOWERS BLOOMING EVERYWHERE.");
+}
+
+function autumn() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("It is MY FAVORITE SEASON.");
+}
